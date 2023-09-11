1af7dd6a10155018f803afbd1e55643384600783
DylanL4556 <Drlott@usca.edu>
2023-09-11 16:52:01 -0400

    Lots of changes to navbar

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const sidebar = document.getElementsByClassName('navbar')[0];
+    const edgeMargin = 50;
+    
+    function showSideBar(){
+        sidebar.classList.add('active');
+    }
+    function hideSidebar(){
+        sidebar.classList.remove('active');
+    }
+    function handleMouseMovement(event){
+        const cursorX = event.clientX;
+        if (cursorX <= edgeMargin){
+            showSideBar();
+        } else {
+            hideSidebar();
+        }
+    }
+    
+    document.addEventListener('mousemove',handleMouseMovement);
+})
